0d23a277564620fc0796f16db7077232eccd7639
WindowsDesktop <ms.crrutgers@gmail.com>
2023-07-25 19:23:58 +0200

    Add starter code for RPA

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RPA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.WriteLine("You get 3 turns to beat the computer.");
+            string move = Console.ReadLine().ToLower();
+            if (move == "rock" | move == "paper" | move == "scissors")
+            {
+                Console.WriteLine(move);
+            }
+            else
+            {
+                Console.WriteLine("invalid move");
+            }
+            
+        }
+    }
+}
